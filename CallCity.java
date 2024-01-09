547e01e712b04fc4bac2cfec15145344b4fd6045
xAm64 <48089283+xAm64@users.noreply.github.com>
2024-01-09 15:01:37 +0100

    Exercice 1.4

--- CallCity.java
+++ CallCity.java
@@ -1,13 +1,14 @@
 package fr.fms.entities;
 
-import org.omg.CORBA.PolicyTypeHelper;
-
 public class CallCity {
 	public static void main(String[] args) {
 		City toulouse = new City("Toulouse", 450000);
 		City bayonne = new City("Bayonne", 180000);
+		City rabat = new City("Rabat", "Maroc", 577000);
 		toulouse.setHabitants(285326);
 		toulouse.setHabitants(450231);
+		rabat.setCountry("Moroc");
+		toulouse.setCountry("France");
 		
 		toulouse.view();
 		bayonne.view();
--- City.java
+++ City.java
@@ -6,11 +6,18 @@ import javax.print.attribute.standard.Chromaticity;
 
 public class City {
 	private String name;
+	private String country;
 	private int habitants;
 	final int MIN_HABITANTS = 0;
-	
+	//constructeurs
+	public City(String name, String country, int population) {
+		this.name = name;
+		this.country = country;
+		this.habitants = population;
+	}
 	public City(String name, int population) {
 		this.name = name;
+		this.country = "unknow";
 		this.habitants = population;
 	}
 	public City(String name) {
@@ -21,13 +28,21 @@ public class City {
 		System.out.println(
 				"Nom: "+ this.name+" "+"Nombre d'habitants "+this.habitants+".");
 	}
-	
+	//setters
 	public void setHabitants(int nombre) {
 		if (nombre > habitants) {
 			this.habitants = nombre;
 			System.out.println("La population de : "+this.name+" est désormait de: "+this.habitants+" habitants.");
 		} else {
 			System.out.println("La ville de: "+this.name+" continue de s'agrandir, sa population ne peut pas baisser");
+		}
+	}
+	public void setCountry(String country) {
+		if (this.country.equals("unknow")) {
+			this.country = country;
+			System.out.println("La ville: "+this.name+". Est désormait en "+this.country+".");
+		} else {
+			System.out.println("La ville: "+this.name+". Est déjà dans un pays: "+this.country+".");
 		}
 	}
 	/*
